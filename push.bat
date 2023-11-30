fd7ac7de24179309dfb514d3d38d35aa906bbf9b
Arron <xfc_arron@163.com>
2023-11-30 22:30:13 +0800

    daily refresh!

--- push.bat
+++ push.bat
@@ -1,7 +1,8 @@
-f:
-cd F:\Obsidian-Vault\Peanut_Notes\Peanut_Notes
-git config --global --unset http.proxy 
-git config --global --unset https.proxy
+
+cd D:\A-Ruosa\PersonalNotes
+
 git add .
 git commit -m "daily refresh!"
 git push
+
+pause
